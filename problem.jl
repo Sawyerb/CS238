602d8362ed51fb8f307916c9559f30c1222f687f
lucy3 <luxisufeili@gmail.com>
2018-11-30 19:34:25 -0800

    it runs

--- problem.jl
+++ problem.jl
@@ -2,26 +2,29 @@ using POMDPs
 using POMDPModels
 using POMDPSimulators
 using POMDPPolicies
-using JLD
+using JLD2, FileIO
+using BeliefUpdaters
 
 start = time()
-solver = QMDPSolver()
+solver = QMDPSolver() 
 pomdp = DonationsPOMDP()
 planner = solve(solver, pomdp)
-save("qmdp_100_10_45_100_100_100_false.jld", "policy", planner)
-
 println("TIME: ", time() - start)
+@save "qmdp_100_10_45_100_100_100_false.jld" planner
+
 println("Starting history.")
+b_up = updater(planner)
+init_dist = initialstate_distribution(pomdp)
 
 hr = HistoryRecorder(max_steps=10)
-hist = simulate(hr, pomdp, planner, (10, 45, 100))
+hist = simulate(hr, pomdp, planner, b_up, init_dist)
 for (s, b, a, r, sp, o) in hist
-    @show s, b, a, r, sp, o
+    @show s, a, r, sp, o
 end
 
-rhist = simulate(hr, pomdp, RandomPolicy(pomdp))
-println("""
-    Cumulative Undiscounted Reward (for 1 simulation)
-        Random: $(undiscounted_reward(rhist))
-        QMDP: $(undiscounted_reward(hist))
-    """)
+#rhist = simulate(hr, pomdp, RandomPolicy(pomdp))
+#println("""
+#    Cumulative Undiscounted Reward (for 1 simulation)
+#        Random: $(undiscounted_reward(rhist))
+#        QMDP: $(undiscounted_reward(hist))
+#    """)
